e0a9a0b2d92c1721f3132cb3e71f81338b2a5acd
AlexKhom3003 <alexandrkhom3003@gmail.com>
2025-09-02 15:12:45 +0200

    update

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -3,7 +3,7 @@ package com.library.model;
 import java.util.Objects;
 
 public class Book {
-    private String String;
+    private String title;
     private String author;
     private String publishedYear;
     private String ISBN;
@@ -12,16 +12,16 @@ public class Book {
     public Book() {
     }
 
-    public Book(String string, String author, String publishedYear, String ISBN, int availableCopies) {
-        String = string;
+    public Book(String title, String author, String publishedYear, String ISBN, int availableCopies) {
+        this.title = title;
         this.author = author;
         this.publishedYear = publishedYear;
         this.ISBN = ISBN;
         this.availableCopies = availableCopies;
     }
 
-    public String getString() {
-        return String;
+    public String getTitle() {
+        return title;
     }
 
     public String getAuthor() {
@@ -29,19 +29,23 @@ public class Book {
     }
 
     public String getPublishedYear() {
+
         return publishedYear;
     }
 
     public String getISBN() {
+
         return ISBN;
     }
 
     public int getAvailableCopies() {
+
         return availableCopies;
     }
 
-    public void setString(String string) {
-        String = string;
+    public void setString(String title) {
+
+        this.title = title;
     }
 
     public void setAuthor(String author) {
@@ -49,33 +53,41 @@ public class Book {
     }
 
     public void setPublishedYear(String publishedYear) {
+
         this.publishedYear = publishedYear;
     }
 
     public void setISBN(String ISBN) {
+
         this.ISBN = ISBN;
     }
 
     public void setAvailableCopies(int availableCopies) {
+
         this.availableCopies = availableCopies;
     }
 
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Book book)) return false;
-        return availableCopies == book.availableCopies && Objects.equals(String, book.String) && Objects.equals(author, book.author) && Objects.equals(publishedYear, book.publishedYear) && Objects.equals(ISBN, book.ISBN);
+        if (!(o instanceof Book book))
+            return false;
+        return availableCopies == book.availableCopies &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(publishedYear, book.publishedYear) &&
+                Objects.equals(ISBN, book.ISBN);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(String, author, publishedYear, ISBN, availableCopies);
+        return Objects.hash(title, author, publishedYear, ISBN, availableCopies);
     }
 
     @Override
     public String toString() {
         return "Book{" +
-                "String='" + String + '\'' +
+                "String='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", publishedYear='" + publishedYear + '\'' +
                 ", ISBN='" + ISBN + '\'' +
--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -1,4 +1,20 @@
 package com.library.repository;
 
-public class BookRepository {
+import com.library.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository {
+    void save(Book book);
+
+    void delete(Book book);
+
+    Optional<Book> findByISBN(String isbn);
+
+    List<Book> findByTitle(String title);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findAll();
 }
